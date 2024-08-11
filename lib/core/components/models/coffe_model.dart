2688145f26b1adaab6e07de1a9d48ef9c84343f2
Dina Ahmed <denaahmed340@gmail.com>
2024-08-12 02:25:57 +0300

    create model

---
+++ lib/core/components/models/coffe_model.dart
@@ -0,0 +1,15 @@
+class CoffeModel {
+  final String name ;
+final String description;
+final String price; 
+final String region;
+final String weight;
+final String flavorProfile;
+final String grindOption;
+final String roastLevel;
+final String imageUrl;
+
+  CoffeModel(this.description, this.price, this.region, this.weight, this.flavorProfile, this.grindOption, this.roastLevel, this.imageUrl, {required this.name});
+
+
+}
