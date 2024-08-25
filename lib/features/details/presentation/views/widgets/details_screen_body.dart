4518143882e52d43549c8280b0b547eff74e9b29
Dina Ahmed <denaahmed340@gmail.com>
2024-08-26 01:56:50 +0300

    done recieve data

--- lib/features/details/presentation/views/widgets/details_screen_body.dart
+++ lib/features/details/presentation/views/widgets/details_screen_body.dart
@@ -1,13 +1,48 @@
+import 'dart:developer';
+
+import 'package:coffe_shop_app/features/details/presentation/controller/detail_cubit/detail_cubit.dart';
+import 'package:coffe_shop_app/features/home/data/models/coffe_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../home/presentation/view/widget/custom_shimmer_placholder.dart';
 
 class DetailsScreenBody extends StatelessWidget {
   const DetailsScreenBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-        child: Text('data'),
-      
+    return BlocBuilder<DetailCubit, DetailState>(builder: (context, state) {
+      if (state is DetailFailureState) {
+        return Scaffold(
+          body: Center(
+            child: Text(state.eMessage),
+          ),
+        );
+        
+      } else if (state is DetailLoadedState) {
+        log('iam body');
+
+        return DetailScreenBodyItem(
+          coffeeModel: state.coffeModel,
+        );
+      } else {
+        log('iam loading');
+        return const ShimmerPlaceholder();
+      }
+    });
+  }
+}
+
+
+class DetailScreenBodyItem extends StatelessWidget {
+  const DetailScreenBodyItem({super.key, required this.coffeeModel});
+  final List<CoffeModel> coffeeModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return  Center(
+      child: Text(coffeeModel[0].name),
     );
   }
 }
